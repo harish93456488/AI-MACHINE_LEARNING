b37884a9e9d554a806bd2599d0e2bf623d2b8692
HARISH R <146081325+harish93456488@users.noreply.github.com>
2024-10-06 19:20:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 st.set_page_config(
     page_title='Palmer Penguins Predictor',
     page_icon='🐧',
 )
 
 st.title('🐧 Palmer Penguins Predictor')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/4dbd79d6ae9f5f97ec9682e79e8ce73c150aca1f/data/penguins_cleaned.csv') 
 
