40404d31658ebd5474c039d2fe8c8fd54a8c2532
HARISH R <146081325+harish93456488@users.noreply.github.com>
2024-10-06 19:27:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,7 @@ st.set_page_config(
 )
 
 st.title('🐧 Palmer Penguins Predictor')
+with st.expander('data'):
+    st.write('**RAW DATA**')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/4dbd79d6ae9f5f97ec9682e79e8ce73c150aca1f/data/penguins_cleaned.csv') 
 df
